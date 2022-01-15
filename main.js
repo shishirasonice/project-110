627b842772fdbb09d15b986ce041d7497a3bbffe
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2022-01-15 14:33:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+prediction = ""
+
 Webcam.set({
     width:350,
     height:350,
@@ -24,3 +26,43 @@ function modelLoaded() {
     console.log('Model Loaded!');
   }
   
+  function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+  function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "It is " + prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+  }
+
+  function gotResult(error, results){
+    if(error){
+      console.error(error);
+    }
+    else{
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+      if(results[0].label == "good"){
+        document.getElementById("update_gesture").innerHTML = "&#128077;";
+      }  
+      if(results[0].label == "victory"){
+        document.getElementById("update_gesture").innerHTML = "&#9996;";
+      }  
+      if(results[0].label == "awesome"){
+        document.getElementById("update_gesture").innerHTML = "&#128076;";
+      }  
+      if(results[0].label == "rock & roll"){
+        document.getElementById("update_gesture").innerHTML = "&#129304;";
+      }  
+      if(results[0].label == "heart"){
+        document.getElementById("update_gesture").innerHTML = "&#65039;";
+      }  
+    }
+  }
